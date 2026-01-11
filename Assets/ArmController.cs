f0d90ea4738faeb0edabedff8a3940f4d1ff2ec7
Martin-B74 <martin.baratte74@gmail.com>
2026-01-11 17:03:26 +0100

    adding fonctionnal MGI

--- Assets/ArmController.cs
+++ Assets/ArmController.cs
@@ -3,11 +3,16 @@ using UnityEngine.UI;
 
 public class RobotController : MonoBehaviour
 {
+    public bool slider = false;
+
+    public Vector3 coordonnees = new Vector3(20f, 0f, 20f);
+
     public GameObject rot1;
     public GameObject rot2;
     public GameObject trans1;
     public GameObject rot3;
     public GameObject trans2;
+    public GameObject pen;
 
     public Slider slider1;
     public Slider slider2;
@@ -20,6 +25,10 @@ public class RobotController : MonoBehaviour
     private translation_1 scriptTrans1;
     private rotation_3 scriptRot3;
     private translation_2 scriptTrans2;
+    private pencil scriptPencil;
+
+    private Vector3 Xmouvement = new Vector3(0.1f, 0f, 0f);
+    private Vector3 Zmouvement = new Vector3(0f, 0f, 0.1f);
 
     void Start()
     {
@@ -28,12 +37,14 @@ public class RobotController : MonoBehaviour
         trans1 = GameObject.Find("Translation1");
         rot3 = GameObject.Find("Rotation3");
         trans2 = GameObject.Find("Translation2");
+        pen = GameObject.Find("Pencil");
 
         scriptRot1 = rot1.GetComponent<rotation_1>();
         scriptRot2 = rot2.GetComponent<rotation_2>();
         scriptTrans1 = trans1.GetComponent<translation_1>();
         scriptRot3 = rot3.GetComponent<rotation_3>();
         scriptTrans2 = trans2.GetComponent<translation_2>();
+        scriptPencil = pen.GetComponent<pencil>();
 
         slider1.value = scriptRot1.q1;
         slider2.value = scriptRot2.q2;
@@ -44,10 +55,50 @@ public class RobotController : MonoBehaviour
 
     void Update()
     {
-        scriptRot1.q1 = slider1.value;
-        scriptRot2.q2 = slider2.value;
-        scriptTrans1.q3 = slider3.value;
-        scriptRot3.q4 = slider4.value;
-        scriptTrans2.q5 = slider5.value;
+        if (slider) {
+            scriptRot1.q1 = slider1.value;
+            scriptRot2.q2 = slider2.value;
+            scriptTrans1.q3 = slider3.value;
+            scriptRot3.q4 = slider4.value;
+            scriptTrans2.q5 = slider5.value;
+        } else {
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                coordonnees += Xmouvement;
+            }
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                coordonnees -= Xmouvement;
+            }
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                coordonnees += Zmouvement;
+            }
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                coordonnees -= Zmouvement;
+            }
+            MGI(coordonnees);
+        }
+    }
+
+    void MGI(Vector3 coordonnees)
+    {
+        Vector3 pos = coordonnees - scriptRot1.transform.position;
+
+        float L = scriptTrans2.q5 + scriptPencil.B;
+
+        float q1 = Mathf.Atan2(pos.x, pos.z) * Mathf.Rad2Deg;
+
+        float R = Mathf.Sqrt(pos.x * pos.x + pos.z * pos.z);
+
+        float dy = pos.y + L - scriptRot2.A;
+
+        float q3 = Mathf.Sqrt(R * R + dy * dy);
+
+        float q2 = Mathf.Atan2(R, dy) * Mathf.Rad2Deg;
+
+        float q4 = 180f - q2;
+
+        scriptRot1.q1 = q1;
+        scriptRot2.q2 = q2;
+        scriptTrans1.q3 = q3;
+        scriptRot3.q4 = q4;
     }
 }
--- Assets/rotation_2.cs
+++ Assets/rotation_2.cs
@@ -8,6 +8,6 @@ public class rotation_2 : MonoBehaviour
     void Update()
     {
         transform.localPosition = new Vector3(0f, A, 0f);
-        transform.localEulerAngles = new Vector3(-q2, 0f, 0f);
+        transform.localEulerAngles = new Vector3(q2, 0f, 0f);
     }
 }
