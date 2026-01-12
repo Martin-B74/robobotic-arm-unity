c7ddb098c865a3a7d1bc6bc81f71e71c67e69b82
Martin-B74 <martin.baratte74@gmail.com>
2026-01-12 12:59:51 +0100

    adding fixes for the coordinates system

--- Assets/EndEffectorDisplay.cs
+++ Assets/EndEffectorDisplay.cs
@@ -10,9 +10,11 @@ public class EndEffectorDisplay : MonoBehaviour
     public GameObject rot2;
     private rotation_2 scriptRot2;
 
+    public GameObject rot3;
+    private rotation_3 scriptRot3;
+
     public GameObject pen;
     private pencil scriptPencil;
-    private Vector3 offsetB;
 
     public ArmController controller;
 
@@ -21,15 +23,18 @@ public class EndEffectorDisplay : MonoBehaviour
         rot2 = GameObject.Find("Rotation2");
         scriptRot2 = rot2.GetComponent<rotation_2>();
 
+        rot3 = GameObject.Find("Rotation3");
+        scriptRot3 = rot3.GetComponent<rotation_3>();
+
         pen = GameObject.Find("Pencil");
         scriptPencil = pen.GetComponent<pencil>();
     }
 
     void Update()
     {
-        offsetB = new Vector3(0f, scriptPencil.B, 0f);
+        Vector3 offsetB = new Vector3(0f, scriptPencil.B * Mathf.Cos((scriptRot2.q2 + scriptRot3.q4) * Mathf.Deg2Rad), 0f);
 
-        Vector3 unityPos = pencil.position - offsetB;
+        Vector3 unityPos = pencil.position + offsetB;
         unityPositionText.text = $"Unity : {unityPos.ToString("F3")}";
 
         Vector3 modelPos = ComputeModelPosition();
@@ -44,9 +49,15 @@ public class EndEffectorDisplay : MonoBehaviour
         float q4 = controller.Q4;
         float q5 = controller.Q5;
 
-        float x = q3 * Mathf.Sin(q2 * Mathf.Deg2Rad) * Mathf.Cos(q1 * Mathf.Deg2Rad);
-        float y = scriptRot2.A + q3 * Mathf.Cos(q2 * Mathf.Deg2Rad) - (q5 + scriptPencil.B) + 10f;
-        float z = q3 * Mathf.Sin(q2 * Mathf.Deg2Rad) * Mathf.Sin(q1 * Mathf.Deg2Rad);
+        float A = scriptRot2.A;
+        float B = scriptPencil.B;
+
+        float L = q5 + B;
+        float R = q3 * Mathf.Sin(q2 * Mathf.Deg2Rad) + L * Mathf.Sin((q2 + q4) * Mathf.Deg2Rad);
+
+        float x = R * Mathf.Sin(q1 * Mathf.Deg2Rad);
+        float y = A + q3 * Mathf.Cos(q2 * Mathf.Deg2Rad) + L * Mathf.Cos((q2 + q4) * Mathf.Deg2Rad) + 10f;
+        float z = R * Mathf.Cos(q1 * Mathf.Deg2Rad);
 
         return new Vector3(x, y, z);
     }
